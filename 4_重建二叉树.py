87f190e42ff1f38a61093137088eec412529b2b6
heihuhuRay <178200901@qq.com>
2017-08-24 18:21:36 +0200

    Q4 pending

--- 4_重建二叉树.py
+++ 4_重建二叉树.py
@@ -4,7 +4,23 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
+class TreeNode(object):
+    def __init__(self, data, left_child=None, right_child=None):
+        self.val = data
+        self.left = left_child
+        self.right = right_child
+
+class Tree(object):
+    def __init__(self):
+        self.root = TreeNode()
+        self.queue = []
+    
+    def add(self, data):
+        node = TreeNode(data)
+        
+
+
 class Solution:
     # 返回构造的TreeNode根节点
     def reConstructBinaryTree(self, pre, tin):
-        # write code here
+        # write code heres
